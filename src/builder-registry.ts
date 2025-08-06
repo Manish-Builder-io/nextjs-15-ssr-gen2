d4d9f32a58e76b1e1f8e8f251f1ce344abe560e9
Manish-Builder-io <manish@builder.io>
2025-08-06 13:50:20 +0530

    Builder integration

---
+++ src/builder-registry.ts
@@ -0,0 +1,16 @@
+"use client";
+import type { RegisteredComponent } from "@builder.io/sdk-react";
+import Counter from "./components/Counter/Counter";
+
+export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
+  {
+    component: Counter,
+    name: "Counter",
+    inputs: [
+      {
+        name: "initialCount",
+        type: "number",
+      },
+    ],
+  },
+];
